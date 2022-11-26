2d11e091c3fc392d0707befbf4ffdfa4f3a6b431
Nathan Stanley <chemapfours@gmail.com>
2022-11-25 20:33:20 -0500

    Reorganization

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-from board.board import Board
-from dice.dice import rollDice
-from screen.boggle_screen import Boggle_Screen
+from game.game import Game
 
 BOARD_DIMENSIONS = 5
 
-class Game:
-    def __init__(self):
-        self.board = Board(rollDice(BOARD_DIMENSIONS))
-        self.screen = Boggle_Screen()
-    def tick(self):
-        self.screen.do_events()
-        self.screen.tick(self.board)
-
 def main():
-    game = Game()
+    game = Game(BOARD_DIMENSIONS)
     while True:
         game.tick()
 
