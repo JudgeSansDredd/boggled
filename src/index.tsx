fe98608ac3efbf8069760690c70bfdaccc0428ee
Nathan Stanley <nathan@pixelparasol.com>
2024-04-03 17:51:35 -0400

    Remove react query

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,10 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Board from './Pages/Board';
 import './index.css';
 
-const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <Board />
-            <ReactQueryDevtools />
-        </QueryClientProvider>
+        <Board />
     </React.StrictMode>
 );
